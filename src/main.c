283b843ee8522e804099a72d295faa6d5cf00284
William A. Kennington III <william@wkennington.com>
2018-01-15 16:08:41 -0800

    main: Cleanup

--- src/main.c
+++ src/main.c
@@ -20,10 +20,12 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "array.h"
 #include "config.h"
 #include "execinfo.h"
 #include "log.h"
 #include "path.h"
+#include "string-util.h"
 
 #define MAIN_BINARY "cc-wrapper"
 
@@ -56,17 +58,17 @@ void set_log_level()
 	LOG_INFO("Log level: %s\n", log_level_to_string(log_level));
 }
 
-void print_args(char *const *args)
+void print_args(char *const *args, enum log_level log_level)
 {
 	for (size_t i = 0; args[i] != NULL; ++i)
-		LOG_INFO("  %s\n", args[i]);
-	LOG_INFO("\n");
+		log_printf(log_level, "  %s\n", args[i]);
+	log_printf(log_level, "\n");
 }
 
 void execute(const struct exec_info *exec_info, char *const *args)
 {
 	LOG_INFO("Calling `%s` with arguments:\n", exec_info->path);
-	print_args(args);
+	print_args(args, LOG_LEVEL_INFO);
 	LOG_INFO("####################################\n");
 	execv(exec_info->path, args);
 }
@@ -78,39 +80,73 @@ bool is_main_binary(const char *path)
 
 int main(int argc, char *argv[])
 {
+	int ret = EXIT_FAILURE;
+	struct array *args = NULL;
+
 	set_log_level();
 
+	/* Determine the name of our target executable*/
 	if (argc >= 1 && is_main_binary(argv[0])) {
 		argc -= 1;
 		argv += 1;
 	}
 
-	if (argc < 1)
-		LOG_FATAL("We must have a binary name\n");
-
-	LOG_INFO("Got initial arguments:\n");
-	print_args(argv);
-
-	char **args = malloc(sizeof(char *) * (argc + 1));
-	if (args == NULL)
-		LOG_FATAL("Failed to allocate args list\n");
-
-	for (size_t i = 0; true; ++i) {
-		args[i] = argv[i];
-		if (argv[i] == NULL)
-			break;
+	if (argc < 1) {
+		LOG_ERROR("We must have a binary name\n");
+		goto out;
 	}
+	LOG_DEBUG("Using binary name: %s\n", argv[0]);
 
+	/* Get information about the executable we found */
 	const struct exec_info *exec_info = get_exec_info(exec_infos, argv[0]);
 	if (exec_info == NULL) {
-		free(args);
-		LOG_FATAL("Failed to get exec info for: %s\n", argv[0]);
+		LOG_ERROR("Failed to get exec info for: %s\n", argv[0]);
+		goto out;
+	}
+	print_exec_info(exec_info);
+
+	/* Make a copy of our arguments to manipulate */
+	args = array_init(sizeof(char *), 0);
+	if (args == NULL) {
+		LOG_ERROR("Failed to allocate args list\n");
+		goto out;
 	}
 
-	print_exec_info(exec_info);
-	execute(exec_info, args);
+	for (ssize_t i = 0; i < argc; ++i) {
+		if (!array_resize(args, i + 1)) {
+			LOG_ERROR("Failed to expand args list\n");
+			goto out;
+		}
+		char **args_data = array_data(args);
+		args_data[i] = string_clone(argv[i]);
+	}
+	{
+		if (!array_resize(args, argc + 1)) {
+			LOG_ERROR("Failed to expand args list\n");
+			goto out;
+		}
+		char **args_data = array_data(args);
+		args_data[argc] = NULL;
+	}
+
+	/* Print out some info to help debugging */
+	LOG_INFO("Got initial arguments:\n");
+	print_args(array_data(args), LOG_LEVEL_INFO);
+
+	/* Execute handlers for rewriting the environment */
+	// TODO(wak)
+
+	execute(exec_info, array_data(args));
 
 	/* We should never reach here */
-	free(args);
-	return EXIT_FAILURE;
+	LOG_FATAL("Went past exec!\n");
+
+out:
+	if (args != NULL) {
+		char **args_data = array_data(args);
+		for (size_t i = 0; i < array_nelems(args); ++i)
+			free(args_data[i]);
+	}
+	array_free(args);
+	return ret;
 }
