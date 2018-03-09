decfcaec20e304e560e92d0eb5a4f2067627c437
William A. Kennington III <william@wkennington.com>
2018-03-08 19:12:35 -0800

    mod_test_util: Add environment and execinfo handling

--- src/mod_test_util.hh
+++ src/mod_test_util.hh
@@ -17,24 +17,51 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include <string_view>
 
+#include "arguments.h"
 #include "environment.h"
+#include "execinfo.h"
 
 static const char *const empty_arr[] = {nullptr};
 
 class ModTest : public ::testing::Test {
  protected:
+  struct arguments *args;
   struct environment *env;
 
   void SetUp() override {
   	::testing::Test::SetUp();
 
+    args = arguments_from_array(empty_arr);
     env = environment_from_array(empty_arr);
+
+    exec_info.name = nullptr;
+    exec_info.path = nullptr;
+    exec_info.package = nullptr;
+    exec_info.type = nullptr;
+    exec_info.prefer = 0;
   }
 
   void TearDown() override {
   	::testing::Test::TearDown();
 
+    arguments_free(args);
     environment_free(env);
+    delete[] exec_info.type;
   }
+
+  void SetExecType(std::string_view str) {
+    delete[] exec_info.type;
+    exec_info.type = new char[str.size() + 1];
+    memcpy(exec_info.type, str.data(), str.size());
+    exec_info.type[str.size()] = '\0';
+  }
+
+  const struct exec_info &GetExecInfo() {
+    return exec_info;
+  }
+
+ private:
+  struct exec_info exec_info;
 };
