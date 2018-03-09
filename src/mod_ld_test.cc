17c28a849b6e795b2e00d2fa01170537ea3a39f3
William A. Kennington III <william@wkennington.com>
2018-03-08 20:33:28 -0800

    mod_ld_test: Cleanup using c++ style argument handling

--- src/mod_ld_test.cc
+++ src/mod_ld_test.cc
@@ -48,256 +48,284 @@ class ModLdTest : public ::ModTest {
 TEST_F(ModLdTest, TestUserArgsEmpty) {
   struct ld_args ld_args;
   EXPECT_TRUE(ld_args_init(args, &ld_args));
-  EXPECT_EQ(0u, arguments_nelems(args));
+
   EXPECT_EQ(0u, ld_args.user_args_start);
   EXPECT_EQ(0u, ld_args.user_args_end);
+  ExpectArgs({});
 }
 
 TEST_F(ModLdTest, TestUserArgsNoMarkers) {
-  EXPECT_TRUE(arguments_insert(args, 0, "-g"));
-  EXPECT_TRUE(arguments_insert(args, 1, "-llib"));
+  AppendArgs({"ld", "-g", "-llib"});
 
   struct ld_args ld_args;
   EXPECT_TRUE(ld_args_init(args, &ld_args));
-  EXPECT_EQ(2u, arguments_nelems(args));
+
   EXPECT_EQ(0u, ld_args.user_args_start);
-  EXPECT_EQ(2u, ld_args.user_args_end);
-  EXPECT_EQ(string_view("-g"), arguments_get(args, 0));
-  EXPECT_EQ(string_view("-llib"), arguments_get(args, 1));
+  EXPECT_EQ(3u, ld_args.user_args_end);
+  ExpectArgs({"ld", "-g", "-llib"});
 }
 
 TEST_F(ModLdTest, TestUserArgsNoEnd) {
-  EXPECT_TRUE(arguments_insert(args, 0, "-g"));
-  EXPECT_TRUE(arguments_insert(args, 1, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 2, "-llib"));
+  AppendArgs({"ld", "-g", CC_WRAPPER_USER_ARGS_BEGIN, "-llib"});
 
   struct ld_args ld_args;
   EXPECT_TRUE(ld_args_init(args, &ld_args));
-  EXPECT_EQ(2u, arguments_nelems(args));
-  EXPECT_EQ(1u, ld_args.user_args_start);
-  EXPECT_EQ(2u, ld_args.user_args_end);
-  EXPECT_EQ(string_view("-g"), arguments_get(args, 0));
-  EXPECT_EQ(string_view("-llib"), arguments_get(args, 1));
+
+  EXPECT_EQ(2u, ld_args.user_args_start);
+  EXPECT_EQ(3u, ld_args.user_args_end);
+  ExpectArgs({"ld", "-g", "-llib"});
 }
 
 TEST_F(ModLdTest, TestUserArgsNoBegin) {
-  EXPECT_TRUE(arguments_insert(args, 0, "-g"));
-  EXPECT_TRUE(arguments_insert(args, 1, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 2, "-llib"));
+  AppendArgs({"ld", "-g", CC_WRAPPER_USER_ARGS_END, "-llib"});
 
   struct ld_args ld_args;
   EXPECT_TRUE(ld_args_init(args, &ld_args));
-  EXPECT_EQ(2u, arguments_nelems(args));
+
   EXPECT_EQ(0u, ld_args.user_args_start);
-  EXPECT_EQ(1u, ld_args.user_args_end);
-  EXPECT_EQ(string_view("-g"), arguments_get(args, 0));
-  EXPECT_EQ(string_view("-llib"), arguments_get(args, 1));
+  EXPECT_EQ(2u, ld_args.user_args_end);
+  ExpectArgs({"ld", "-g", "-llib"});
 }
 
 TEST_F(ModLdTest, TestUserArgsInvalid) {
-  EXPECT_TRUE(arguments_insert(args, 0, "-g"));
-  EXPECT_TRUE(arguments_insert(args, 1, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 2, "-llib"));
-  EXPECT_TRUE(arguments_insert(args, 3, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 4, "-d"));
+  AppendArgs({
+      "ld",
+      CC_WRAPPER_USER_ARGS_END,
+      "-llib",
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-d",
+  });
 
   struct ld_args ld_args;
   EXPECT_FALSE(ld_args_init(args, &ld_args));
 }
 
 TEST_F(ModLdTest, TestUserArgsTypical) {
-  EXPECT_TRUE(arguments_insert(args, 0, "-g"));
-  EXPECT_TRUE(arguments_insert(args, 1, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 2, "-llib"));
-  EXPECT_TRUE(arguments_insert(args, 3, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 4, "-d"));
+  AppendArgs({
+      "ld",
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-llib",
+      CC_WRAPPER_USER_ARGS_END,
+      "-d",
+  });
 
   struct ld_args ld_args;
   EXPECT_TRUE(ld_args_init(args, &ld_args));
-  EXPECT_EQ(3u, arguments_nelems(args));
+
   EXPECT_EQ(1u, ld_args.user_args_start);
   EXPECT_EQ(2u, ld_args.user_args_end);
-  EXPECT_EQ(string_view("-g"), string_view(arguments_get(args, 0)));
-  EXPECT_EQ(string_view("-llib"), string_view(arguments_get(args, 1)));
-  EXPECT_EQ(string_view("-d"), string_view(arguments_get(args, 2)));
+  ExpectArgs({"ld", "-llib", "-d"});
 }
 
 TEST_F(ModLdTest, TestUserArgsInsert) {
-  EXPECT_TRUE(arguments_insert(args, 0, "-g"));
-  EXPECT_TRUE(arguments_insert(args, 1, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 2, "-llib"));
-  EXPECT_TRUE(arguments_insert(args, 3, "-llib2"));
-  EXPECT_TRUE(arguments_insert(args, 4, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 5, "-d"));
+  AppendArgs({
+      "ld",
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-llib1",
+      "-llib2",
+      CC_WRAPPER_USER_ARGS_END,
+      "-d",
+  });
 
   struct ld_args ld_args;
   EXPECT_TRUE(ld_args_init(args, &ld_args));
   EXPECT_FALSE(ld_args_insert(&ld_args, 2, "-Oinvalid"));
   EXPECT_TRUE(ld_args_insert(&ld_args, 1, "-O1"));
   EXPECT_TRUE(ld_args_insert(&ld_args, 4, "-O2"));
-  EXPECT_EQ(6u, arguments_nelems(args));
+
   EXPECT_EQ(2u, ld_args.user_args_start);
   EXPECT_EQ(4u, ld_args.user_args_end);
-  EXPECT_EQ(string_view("-g"), string_view(arguments_get(args, 0)));
-  EXPECT_EQ(string_view("-O1"), string_view(arguments_get(args, 1)));
-  EXPECT_EQ(string_view("-llib"), string_view(arguments_get(args, 2)));
-  EXPECT_EQ(string_view("-llib2"), string_view(arguments_get(args, 3)));
-  EXPECT_EQ(string_view("-O2"), string_view(arguments_get(args, 4)));
-  EXPECT_EQ(string_view("-d"), string_view(arguments_get(args, 5)));
+  ExpectArgs({
+      "ld",
+      "-O1",
+      "-llib1",
+      "-llib2",
+      "-O2",
+      "-d",
+  });
 }
 
 TEST_F(ModLdTest, TestCheckLd) {
+  AppendArgs({
+      "ld",
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      CC_WRAPPER_USER_ARGS_END,
+  });
+
   SetExecType("cc");
   EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), nullptr, nullptr));
 
-  EXPECT_TRUE(arguments_insert(args, 0, "ld"));
-  EXPECT_TRUE(arguments_insert(args, 1, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 2, CC_WRAPPER_USER_ARGS_END));
   SetExecType("ld");
-  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, nullptr));
+  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, env));
 
-  EXPECT_EQ(1u, arguments_nelems(args));
-  EXPECT_EQ(string_view("ld"), string_view(arguments_get(args, 0)));
+  ExpectArgs({"ld"});
 }
 
 TEST_F(ModLdTest, TestNoDynamicLinker) {
   const char *no_dl = "/no-such-dl";
 
   SetExecType("ld");
-  EXPECT_TRUE(arguments_insert(args, 0, "ld"));
-  EXPECT_TRUE(arguments_insert(args, 1, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 2, no_dl));
-  EXPECT_TRUE(arguments_insert(args, 3, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 4, no_dl));
-  EXPECT_TRUE(arguments_insert(args, 5, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 6, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 7, no_dl));
-  EXPECT_TRUE(arguments_insert(args, 8, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 9, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 10, no_dl));
+  AppendArgs({
+      "ld",
+      "-dynamic-linker",
+      no_dl,
+      "-dynamic-linker",
+      no_dl,
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-dynamic-linker",
+      no_dl,
+      CC_WRAPPER_USER_ARGS_END,
+      "-dynamic-linker",
+      no_dl,
+  });
 
-  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, nullptr));
+  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, env));
 
-  EXPECT_EQ(9u, arguments_nelems(args));
-  EXPECT_EQ(string_view("ld"), string_view(arguments_get(args, 0)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 1)));
-  EXPECT_EQ(string_view(no_dl), string_view(arguments_get(args, 2)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 3)));
-  EXPECT_EQ(string_view(no_dl), string_view(arguments_get(args, 4)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 5)));
-  EXPECT_EQ(string_view(no_dl), string_view(arguments_get(args, 6)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 7)));
-  EXPECT_EQ(string_view(no_dl), string_view(arguments_get(args, 8)));
+  ExpectArgs({
+      "ld",
+      "-dynamic-linker",
+      no_dl,
+      "-dynamic-linker",
+      no_dl,
+      "-dynamic-linker",
+      no_dl,
+      "-dynamic-linker",
+      no_dl,
+  });
 }
 
 TEST_F(ModLdTest, TestDynamicLinker) {
   const char *no_dl = "/no-such-dl";
 
   SetExecType("ld");
-  EXPECT_TRUE(arguments_insert(args, 0, "ld"));
-  EXPECT_TRUE(arguments_insert(args, 1, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 2, no_dl));
-  EXPECT_TRUE(arguments_insert(args, 3, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 4, no_dl));
-  EXPECT_TRUE(arguments_insert(args, 5, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 6, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 7, no_dl));
-  EXPECT_TRUE(arguments_insert(args, 8, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 9, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 10, no_dl));
-
+  AppendArgs({
+      "ld",
+      "-dynamic-linker",
+      no_dl,
+      "-dynamic-linker",
+      no_dl,
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-dynamic-linker",
+      no_dl,
+      CC_WRAPPER_USER_ARGS_END,
+      "-dynamic-linker",
+      no_dl,
+  });
   target_dl = "/bad-dl";
-  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, nullptr));
 
-  EXPECT_EQ(9u, arguments_nelems(args));
-  EXPECT_EQ(string_view("ld"), string_view(arguments_get(args, 0)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 1)));
-  EXPECT_EQ(string_view(target_dl), string_view(arguments_get(args, 2)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 3)));
-  EXPECT_EQ(string_view(target_dl), string_view(arguments_get(args, 4)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 5)));
-  EXPECT_EQ(string_view(no_dl), string_view(arguments_get(args, 6)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 7)));
-  EXPECT_EQ(string_view(no_dl), string_view(arguments_get(args, 8)));
+  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, env));
+
+  ExpectArgs({
+      "ld",
+      "-dynamic-linker",
+      target_dl,
+      "-dynamic-linker",
+      target_dl,
+      "-dynamic-linker",
+      no_dl,
+      "-dynamic-linker",
+      no_dl,
+  });
+}
+
+TEST_F(ModLdTest, TestNoRpathAdding) {
+  SetExecType("ld");
+  AppendArgs({
+      "ld",
+      "-L/path1",
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-L/path2",
+      CC_WRAPPER_USER_ARGS_END,
+      "-L/path3",
+  });
+
+  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, env));
+
+  ExpectArgs({
+      "ld",
+      "-L/path1",
+      "-rpath",
+      "/path1",
+      "-L/path2",
+      "-L/path3",
+      "-rpath",
+      "/path3",
+  });
 }
 
 TEST_F(ModLdTest, TestRpathAdding) {
   SetExecType("ld");
-  EXPECT_TRUE(arguments_insert(args, 0, "ld"));
-  EXPECT_TRUE(arguments_insert(args, 1, "-L/path1"));
-  EXPECT_TRUE(arguments_insert(args, 2, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 3, "-L/path2"));
-  EXPECT_TRUE(arguments_insert(args, 4, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 5, "-L/path3"));
+  AppendArgs({
+      "ld",
+      "-L/path1",
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-L/path2",
+      CC_WRAPPER_USER_ARGS_END,
+      "-L/path3",
+  });
 
-  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, nullptr));
+  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, env));
 
-  EXPECT_EQ(8u, arguments_nelems(args));
-  EXPECT_EQ(string_view("ld"), string_view(arguments_get(args, 0)));
-  EXPECT_EQ(string_view("-L/path1"), string_view(arguments_get(args, 1)));
-  EXPECT_EQ(string_view("-rpath"), string_view(arguments_get(args, 2)));
-  EXPECT_EQ(string_view("/path1"), string_view(arguments_get(args, 3)));
-  EXPECT_EQ(string_view("-L/path2"), string_view(arguments_get(args, 4)));
-  EXPECT_EQ(string_view("-L/path3"), string_view(arguments_get(args, 5)));
-  EXPECT_EQ(string_view("-rpath"), string_view(arguments_get(args, 6)));
-  EXPECT_EQ(string_view("/path3"), string_view(arguments_get(args, 7)));
+  ExpectArgs({
+      "ld",
+      "-L/path1",
+      "-rpath",
+      "/path1",
+      "-L/path2",
+      "-L/path3",
+      "-rpath",
+      "/path3",
+  });
 }
 
 TEST_F(ModLdTest, TestLibcPathAdding) {
   SetExecType("ld");
-  EXPECT_TRUE(arguments_insert(args, 0, "ld"));
-  EXPECT_TRUE(arguments_insert(args, 1, "-d1"));
-  EXPECT_TRUE(arguments_insert(args, 2, CC_WRAPPER_USER_ARGS_BEGIN));
-  EXPECT_TRUE(arguments_insert(args, 3, "-d2"));
-  EXPECT_TRUE(arguments_insert(args, 4, CC_WRAPPER_USER_ARGS_END));
-  EXPECT_TRUE(arguments_insert(args, 5, "-d3"));
-
+  AppendArgs({
+      "ld",
+      "-d1",
+      CC_WRAPPER_USER_ARGS_BEGIN,
+      "-d2",
+      CC_WRAPPER_USER_ARGS_END,
+      "-d3",
+  });
   target_libc_dynamic_libs = "/path-dynamic";
   target_libc_static_libs = "/path-static";
-  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, nullptr));
 
-  EXPECT_EQ(10u, arguments_nelems(args));
-  EXPECT_EQ(string_view("ld"), string_view(arguments_get(args, 0)));
-  EXPECT_EQ(string_view("-d1"), string_view(arguments_get(args, 1)));
-  EXPECT_EQ(string_view("-d2"), string_view(arguments_get(args, 2)));
-  EXPECT_EQ(string_view("-d3"), string_view(arguments_get(args, 3)));
-  EXPECT_EQ(string("-L") + target_libc_dynamic_libs,
-            string_view(arguments_get(args, 4)));
-  EXPECT_EQ(string_view("-rpath"), string_view(arguments_get(args, 5)));
-  EXPECT_EQ(string_view(target_libc_dynamic_libs),
-            string_view(arguments_get(args, 6)));
-  EXPECT_EQ(string("-L") + target_libc_static_libs,
-            string_view(arguments_get(args, 7)));
-  EXPECT_EQ(string_view("-rpath"), string_view(arguments_get(args, 8)));
-  EXPECT_EQ(string_view(target_libc_static_libs),
-            string_view(arguments_get(args, 9)));
+  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, env));
+
+  ExpectArgs({
+      "ld",
+      "-d1",
+      "-d2",
+      "-d3",
+      string("-L") + target_libc_dynamic_libs,
+      "-rpath",
+      target_libc_dynamic_libs,
+      string("-L") + target_libc_static_libs,
+      "-rpath",
+      target_libc_static_libs,
+  });
 }
 
 TEST_F(ModLdTest, TestUserArgsOnly) {
   SetExecType("ld");
-  EXPECT_TRUE(arguments_insert(args, 0, "ld"));
-  EXPECT_TRUE(arguments_insert(args, 1, "-dynamic-linker"));
-  EXPECT_TRUE(arguments_insert(args, 2, "/no-such-dl"));
-  EXPECT_TRUE(arguments_insert(args, 3, "-L/no-such-path"));
-  EXPECT_TRUE(arguments_insert(args, 4, "-d3"));
+  AppendArgs({
+      "ld",
+      "-dynamic-linker",
+      "/no-such-dl",
+      "-L/no-such-path",
+      "-d3",
+  });
 
-  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, nullptr));
-  EXPECT_EQ(5u, arguments_nelems(args));
-  EXPECT_EQ(string_view("ld"), string_view(arguments_get(args, 0)));
-  EXPECT_EQ(string_view("-dynamic-linker"),
-            string_view(arguments_get(args, 1)));
-  EXPECT_EQ(string_view("/no-such-dl"), string_view(arguments_get(args, 2)));
-  EXPECT_EQ(string_view("-L/no-such-path"),
-            string_view(arguments_get(args, 3)));
-  EXPECT_EQ(string_view("-d3"), string_view(arguments_get(args, 4)));
+  EXPECT_TRUE(mod_ld_rewrite(&GetExecInfo(), args, env));
+
+  ExpectArgs({
+      "ld",
+      "-dynamic-linker",
+      "/no-such-dl",
+      "-L/no-such-path",
+      "-d3",
+  });
 }
 
 }  // namespace
